ea3bbf6fd3c846ead986d002973bc235f2cba5cd
V-Saurus <v-saurus@yandex.ru>
2019-06-10 15:35:15 +0300

    10.06.2019
    1. Доработана инициализация.
    2. Исправлен приём информации из порта.
       При отсутствии информации из порта в течение времени задержки считывается 1 символ, остальная информация считывается на следующем шаге.

--- main.c
+++ main.c
@@ -66,23 +66,28 @@ int open_port(void) {
     cfsetispeed(&options, B115200);
     cfsetospeed(&options, B115200);
 
+    cfmakeraw(&options);
+
+    options.c_cflag &= ~(CBAUD | CSTOPB);
+    options.c_cflag |= (B115200 | CLOCAL | CREAD);
+
     /* разрешение приёмника и установка локального режима */
-    options.c_cflag |= (CLOCAL | CREAD);
+//    options.c_cflag |= (CLOCAL | CREAD);
 
     /* установка контроля чётности и размеров символов */
-    options.c_cflag &= ~PARENB;
-    options.c_cflag &= ~CSTOPB;
-    options.c_cflag &= ~CSIZE;
-    options.c_cflag |= CS8;
+//    options.c_cflag &= ~PARENB;
+//    options.c_cflag &= ~CSTOPB;
+//    options.c_cflag &= ~CSIZE;
+//    options.c_cflag |= CS8;
 
     /* деактивирование аппаратного управления потоком */
 //    options.c_cflag &= ~CNEW_RTSCTS;
 
     /* выбор неканонического (raw) ввода */
-    options.c_cflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+//    options.c_cflag &= ~(ICANON | ECHO | ECHOE | ISIG);
 
     /* выбор необработанного (raw) вывода */
-    options.c_cflag &= ~OPOST;
+//    options.c_cflag &= ~OPOST;
 
     /* установка новыз опций для порта */
     tcsetattr(fd, TCSANOW, &options);
